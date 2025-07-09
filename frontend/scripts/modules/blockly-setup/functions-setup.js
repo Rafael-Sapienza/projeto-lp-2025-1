6e4a50705d76ebea94cadd75701c86d9036023ea
Leandro Lucas <leandrolucas7@hotmail.com>
2025-07-09 00:17:22 -0300

    functions blocks definitions working

--- frontend/scripts/modules/blockly-setup/functions-setup.js
+++ frontend/scripts/modules/blockly-setup/functions-setup.js
@@ -1,4 +1,6 @@
+import { creatorDivHTML } from "../interface/function-creation.js";
 import { createdFunctions } from "./functions-logic.js";
+import { functionsCategory } from "../blocks-definitions/blocks-library.js"
 
 function createFunctionsFlyout(workspace) {
     const xmlList = [];
@@ -13,6 +15,8 @@ function createFunctionsFlyout(workspace) {
     deleteBlockBtn.setAttribute("callbackKey", "DELETE_FUNCTION");
     xmlList.push(deleteBlockBtn);
 
+    // CODE TO GET ALL THE SETTERS AND GETTERS OF FUNCTION!!!!!
+
     return xmlList;
 }
 
@@ -25,105 +29,68 @@ export const setupFunctionsToolbox = function(workspace) {
     );
 
     // Register the button callbacks
-    workspace.registerButtonCallback("CREATE_FUNCTION", function(button) {
-        showCreateFunctionWindow(function(blockDefinition) {
-            const block = createFunctionDefinitionBlock(blockDefinition);
-            Blockly.defineBlocksWithJsonArray([block]);
-            const newBlock = workspace.newBlock(`function_def_${blockDefinition.name.replace(/\s+/g, "_")}`);
-            for (const param of blockDefinition.parameters) {
-              const paramGetterBlockType = `function_param_get_${blockDefinition.name}_${param.name}`;
-              const paramBlock = workspace.newBlock(paramGetterBlockType);
-              paramBlock.initSvg();
-              paramBlock.render();
-            }
-            newBlock.initSvg();
-            newBlock.render();
+    workspace.registerButtonCallback("CREATE_FUNCTION", button => {
+        showCreateFunctionWindow(blockDefinition => {
+            const blockSuffix = blockDefinition.name.replace(/\s+/g, "_");
+            defineFunctionBlocks(blockDefinition);
+            renderFunctionBlocks(workspace, blockSuffix)
         });
     });
 
-    workspace.registerButtonCallback("DELETE_FUNCTION", function(button) {
+    workspace.registerButtonCallback("DELETE_FUNCTION", button => {
         console.log("implement block deletion");
     });
 }
 
 
 function showCreateFunctionWindow(callback) {
-    const mainUI = document.querySelector(".main-container");  // use your actual UI wrapper
+    const mainContainer = document.querySelector(".main-container");  
     const parentDiv = document.querySelector("#create-functions-layer");
 
     const creatorDiv = document.createElement("div");
     creatorDiv.id = "function-creator";
-    creatorDiv.innerHTML = `
-        <div class="form-content">
-            <!-- your existing form here -->
-            <div class="form-line">
-                <label>Nome do Bloco:</label>
-                <input id="function-name" type="text" />
-            </div>
-            <div class="form-line">
-                <label>Adicionar Caixas:</label>
-                <select id="function-parameters">
-                    <option value="NONE">Opcional</option>
-                    <option value="Number">Número</option>
-                    <option value="String">Texto</option>
-                    <option value="Boolean">Verdadeiro/Falso</option>
-                </select>
-            </div>
-            <div id="parameters-container"></div>
-            <div class="form-line">
-                <label>Devolver:</label>
-                <select id="function-return">
-                    <option value="NONE">Opcional</option>
-                    <option value="Number">Número</option>
-                    <option value="String">Texto</option>
-                    <option value="Boolean">Verdadeiro/Falso</option>
-                </select>
-            </div>
-            <div class="form-line">
-                <button id="cancel-creation">Cancelar</button>
-                <button id="confirm-creation">Criar</button>
-            </div>
-        </div>
-    `;
+    creatorDiv.innerHTML = creatorDivHTML;
     parentDiv.appendChild(creatorDiv);
+    toggleNodeActivation(mainContainer);
 
-    // ➕ Blur background
-    if (mainUI) mainUI.classList.add("blur");
-
-    // 🗑️ Handle Cancel button
+    // Cancel creation logic
     const cancelBtn = creatorDiv.querySelector("#cancel-creation");
     cancelBtn.addEventListener("click", () => {
-        creatorDiv.remove();                        // remove modal
-        if (mainUI) mainUI.classList.remove("blur"); // unblur background
+        creatorDiv.remove();                        
+        toggleNodeActivation(mainContainer);
     });
 
-    // ✨ Dynamic param handling
-    const select = creatorDiv.querySelector("#function-parameters");
+    // Handle parameters logic
+    const parametersSelect = creatorDiv.querySelector("#function-parameters");
     const parametersContainer = creatorDiv.querySelector("#parameters-container");
 
-    select.addEventListener("change", () => {
-        const type = select.value;
-        const name = select.options[select.selectedIndex].innerText;
-        if (type === "NONE") return;
+    // Adding a new parameter
+    parametersSelect.addEventListener("change", () => {
+        const selectType = parametersSelect.value;
+        if (selectType === "NONE") {
+            return;
+        }
 
         const parametersDiv = document.createElement("div");
-        //const parametersItems = parametersContainer.querySelectorAll(".parameter-item");
-        //const parametersCount = parametersItems ? parametersItems.length + 1 : 1;
-        const parametersCount = parametersContainer.querySelectorAll(".parameter-item").length + 1;        parametersDiv.classList.add("parameter-item");
+        parametersDiv.classList.add("parameter-item");
         parametersDiv.innerHTML = `
-            <label>## Caixa ${String(parametersCount).padStart(2, '0')} [${name}]:</label>
-            <input type="text" data-type="${type}"/>
-            <button class="delete-parameter">🗑️</button>
+            <label>
+                <img src="${getTypeIconPath(selectType)}" class="parameter-icon"/>
+            </label>
+            <input type="text" data-type="${selectType}"/>
+            <label>
+                <img src="${getTypeIconPath("delete-parameter")}" class="delete-parameter"/>
+            </label>
         `;
+        // Add the parameter and reset the parameter input element
         parametersContainer.appendChild(parametersDiv);
-        select.value = "NONE";
+        parametersSelect.value = "NONE";
     });
 
+    // Delete parameter
     parametersContainer.addEventListener("click", (e) => {
         if (e.target.classList.contains("delete-parameter")) {
-            console.log(e.target);
-            console.log(e.target.parentElement)
-            e.target.parentElement.remove();
+            e.target.closest(".parameter-item")?.remove();
         }
     });
 
@@ -131,74 +98,61 @@ function showCreateFunctionWindow(callback) {
     // Submission
     const createBtn = creatorDiv.querySelector("#confirm-creation");
     createBtn.addEventListener("click", e => {
-        const nameInput = creatorDiv.querySelector("#function-name");
-        const blockName = nameInput.value;
+        const functionNameInput = creatorDiv.querySelector("#function-name");
+        const blockName = functionNameInput.value;
         if (!blockName || blockName in createdFunctions) {
             const placeholder = blockName ? "bloco já existe." : "dê um nome ao bloco.";
-            nameInput.value = "";
-            nameInput.classList.add("invalid-function-name");
-            nameInput.placeholder = placeholder;
+            highlighInvalidInput(functionNameInput, placeholder, 1000);
+            return;
+        }
 
-            setTimeout(() => {
-            nameInput.classList.remove("invalid-function-name");
-                nameInput.placeholder = "";
-            }, 1000);
-        }
-        else {
-            const blockDefinition = {
-                name: null,
-                parameters: [],
-                return: null,
-                addedToWorkspace: null,
-            };
-            const parametersItems = parametersContainer.querySelectorAll(".parameter-item");
-            for (const parameter of parametersItems) {
-                const parameterInput = parameter.children[1];
-                const parameterName = parameterInput.value;
-                const parameterType = parameterInput.dataset.type;
-                if (!parameterName || blockDefinition.parameters.some(param => param.name === parameterName)) {
-                    const placeholder = parameterName ? "nome repetido" : "caixa sem nome";
-                    parameterInput.value = "";
-                    parameterInput.classList.add("invalid-function-name");
-                    parameterInput.placeholder = placeholder;
-                    setTimeout(() => {
-                        parameterInput.classList.remove("invalid-function-name");
-                        parameterInput.placeholder = "";
-                    }, 1000);
-                    return;
-                }
-                const returnSelect = creatorDiv.querySelector("#function-return");
-                blockDefinition.parameters.push({name: parameterName, type: parameterType});
-                blockDefinition.return = returnSelect.value === "NONE" ? null : returnSelect.value;
+        const returnSelect = creatorDiv.querySelector("#function-return");
+
+        const blockDefinition = {
+            name: blockName,
+            parameters: [],
+            return: returnSelect.value === "NONE" ? null : returnSelect.value,
+            addedToWorkspace: null,
+        };
+        const functionParameters = parametersContainer.querySelectorAll(".parameter-item");
+        for (const parameter of functionParameters) {
+            const parameterInput = parameter.children[1];
+            const parameterName = parameterInput.value;
+            const parameterType = parameterInput.dataset.type;
+            if (!parameterName || blockDefinition.parameters.some(param => param.name === parameterName)) {
+                const placeholder = parameterName ? "nome repetido" : "caixa sem nome";
+                highlighInvalidInput(parameterInput, placeholder, 1000);
+                return;
             }
-            console.log(blockDefinition);
-            blockDefinition.name = blockName;
-            createdFunctions[blockName] = blockDefinition;
-            callback(blockDefinition);
-            creatorDiv.remove();
-            if (mainUI) mainUI.classList.remove("blur");
+            blockDefinition.parameters.push({name: parameterName, type: parameterType});
         }
+        createdFunctions[blockName] = blockDefinition;
+        callback(blockDefinition);
+        creatorDiv.remove();
+        toggleNodeActivation(mainContainer);
     });
 }
 
 
+// Uses the user blockDefinition to build/define the new block on blockly
+function defineFunctionBlocks(blockDefinition) {
+    const argNames = blockDefinition.parameters.map((param, i) => `%${i + 1}`).join(", ");
 
-function createFunctionDefinitionBlock(blockDefinition) {
-    const blockType = `function_def_${blockDefinition["name"].replace(/\s+/g, "_")}`;
+    // DEF block definition
+    const defBlockType = `function_def_${blockDefinition["name"].replace(/\s+/g, "_")}`;
 
-    const args0 = blockDefinition.parameters.map(param => ({
-        type: "input_value",
+    const defArgs0 = blockDefinition.parameters.map(param => ({
+        type: "field_label",
         name: param.name,
+        text: param.name,
         check: param.type
     }));
+    const defMessage0 = `DEFINIR ${blockDefinition.name}: ${argNames}`;
 
-    const argNames = blockDefinition.parameters.map((param, i) => `%${i + 1}`).join(", ");
-    const message0 = `DEFINIR ${blockDefinition.name} ${argNames}`;
-
-    const block = {
-        type: blockType,
-        message0,
-        args0,
+    const defBlock = {
+        type: defBlockType,
+        message0: defMessage0,
+        args0: defArgs0,
         message1: "%1",
         args1: [
             {
@@ -206,47 +160,117 @@ function createFunctionDefinitionBlock(blockDefinition) {
                 name: "BODY"
             }
         ],
-        colour: 230,
-        tooltip: `Define function ${blockDefinition["name"]}`,
+        colour: functionsCategory.colour,
+        tooltip: `Definir o bloco ${blockDefinition["name"]}`,
         helpUrl: ""
     };
 
-    if (blockDefinition.return && blockDefinition.return !== "NONE") {
-        block.message2 = "Devolver %1";
-        block.args2 = [
+    if (blockDefinition.return) {
+        defBlock.message2 = "DEVOLVER %1";
+        defBlock.args2 = [
             {
                 type: "input_value",
                 name: "RETURN",
                 check: blockDefinition.return,
             },
         ];
-        block.output = blockDefinition.return;
-    } else {
-        block.previousStatement = null;
-        block.nextStatement = null;
+        defBlock.output = blockDefinition.return;
     }
 
-    // After defining the function block: =====>>>> ADDED YOUR CHANGE HERE!!!!
-    for (const param of blockDefinition.parameters) {
-      const blockType = `function_param_get_${blockDefinition.name}_${param.name}`;
-      const block = {
-        type: blockType,
-        message0: `%1`,
-        args0: [
-          {
-            type: "field_label",
-            name: "PARAM_NAME",
-            text: param.name,
-          }
-        ],
-        output: param.type,
-        colour: 230,
-        tooltip: `Usa o parâmetro '${param.name}' da função '${blockDefinition.name}'`,
+    // SET block definition
+    const setBlockType = `function_set_${blockDefinition["name"].replace(/\s+/g, "_")}`;
+
+    const setArgs0 = blockDefinition.parameters.map(param => ({
+        type: "input_value",
+        name: param.name,
+        text: param.name,
+        check: param.type
+    }));
+    const setMessage0 = `${blockDefinition.name}: ${argNames}`;
+
+    const setBlock = {
+        type: setBlockType,
+        message0: setMessage0,
+        args0: setArgs0,
+        colour: functionsCategory.colour,
+        tooltip: `Usar o bloco ${blockDefinition["name"]}`,
         helpUrl: ""
-      };
+    };
 
-      Blockly.defineBlocksWithJsonArray([block]);
+    if (blockDefinition.return) {
+        setBlock.output = blockDefinition.return;
+    }
+    else {
+        setBlock.previousStatement = null;
+        setBlock.nextStatement = null;
     }
 
-    return block;
+    Blockly.defineBlocksWithJsonArray([defBlock, setBlock]);
+
+    // GET blocks definitions (for every parameter)
+    for (const param of blockDefinition.parameters) {
+        const blockType = `function_param_get_${blockDefinition.name}_${param.name}`;
+        const getBlock = {
+            type: blockType,
+            message0: `%1`,
+            args0: [
+                {
+                    type: "field_label",
+                    name: "PARAM_NAME",
+                    text: param.name,
+                },
+            ],
+            output: param.type,
+            colour: functionsCategory.colour,
+            tooltip: `Usa o parâmetro '${param.name}' da função '${blockDefinition.name}'`,
+            helpUrl: ""
+        };
+
+        Blockly.defineBlocksWithJsonArray([getBlock]);
+    }
+
+}
+
+function renderFunctionBlocks(workspace, blockSuffix) {
+    const defBlock = workspace.newBlock(`function_def_${blockSuffix}`);
+    defBlock.initSvg();
+    defBlock.render();
+    defBlock.setDeletable(false);
+}
+
+/************ HELPERS *****************/
+export const toggleNodeActivation = function(node) {
+    if (!node) {
+        return;
+    }
+    node.classList.toggle("blur");
+}
+
+export const getTypeIconPath = function(type) {
+    if (type === "String") {
+        return "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='%23664FC2' viewBox='0 0 16 16'%3e%3cpath d='M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105'/%3e%3cpath d='M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5'/%3e%3c/svg%3e";
+    }
+    else if (type === "Number") {
+        return "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='%23664FC2' viewBox='0 0 16 16'%3e%3cpath d='M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z'/%3e%3cpath d='M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z'/%3e%3c/svg%3e";
+    }
+    else if (type === "Boolean") {
+        return "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='%23664FC2' viewBox='0 0 16 16'%3e%3cpath d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z'/%3e%3cpath d='M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z'/%3e%3c/svg%3e";
+    }
+    else if (type === "delete-parameter") {
+        return "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='%23664FC2' viewBox='0 0 16 16'%3e%3cpath d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z'/%3e%3cpath d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z'/%3e%3c/svg%3e";
+    }
+    else {
+        return " ";
+    }
+}
+
+export const highlighInvalidInput = function(inputNode, message, time) {
+    inputNode.value = "";
+    inputNode.classList.add("invalid-function-name");
+    inputNode.placeholder = message;
+
+    setTimeout(() => {
+        inputNode.classList.remove("invalid-function-name");
+        inputNode.placeholder = "";
+    }, time);
 }
