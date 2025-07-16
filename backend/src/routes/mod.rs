48df9a2aea2187b7d7cea30e5538c69b365f5c27
Leandro Lucas <leandrolucas7@hotmail.com>
2025-07-16 16:04:18 -0300

    files names changes

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,7 +1,7 @@
-use crate::handlers::{execute, hard_interpreter};
+use crate::handlers::{easy_execution, hard_interpreter};
 use actix_web::web;
 
 pub fn register_routes(config: &mut web::ServiceConfig) {
-    config.route("/execute", web::post().to(execute::execute));
+    config.route("/execute", web::post().to(easy_execution::execute));
     config.route("/hard-interpreter", web::post().to(hard_interpreter::execute));
 }
